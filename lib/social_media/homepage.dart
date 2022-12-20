eabec3905c21b45f8c11c60ce547f5f38aba95cd
Oliver Munn <oliver_munn@icloud.com>
2022-12-20 15:05:38 +0000

    Added scroll to top button

--- lib/social_media/homepage.dart
+++ lib/social_media/homepage.dart
@@ -4,18 +4,34 @@ import 'post_widget.dart';
 
 class HomePage extends StatelessWidget {
   // Create HomePage Class
-  const HomePage({Key? key, required this.title}) : super(key: key);
+  HomePage({Key? key, required this.title}) : super(key: key);
   final String title; // Define title argument
+  final _scrollController = ScrollController();
 
   @override // Override the existing widget build method
   Widget build(BuildContext context) {
     User user = User("1"); // Create user object with id of 1
     return Scaffold(
       appBar: AppBar(
+        leading: TextButton(
+          // Create a basic button
+          child: const Image(
+            // Set button to an image
+            image: AssetImage("assets/placeholders/320x114.png"),
+          ),
+          onPressed: () {
+            // When image pressed
+            _scrollController.animateTo(0, //Scroll to top in 500ms
+                duration: const Duration(milliseconds: 500),
+                curve: Curves
+                    .fastOutSlowIn); //Start scrolling fast then slow down when near top
+          },
+        ),
         title: Text(title),
       ),
       body: Center(
           child: ListView.separated(
+        controller: _scrollController,
         // Create a list of post widgets
         padding: const EdgeInsets.all(
             8), // Add padding to list so doesn't overflow to sides of screen
--- lib/tab_navigator.dart
+++ lib/tab_navigator.dart
@@ -16,7 +16,7 @@ class TabNavigator extends StatelessWidget {
   Map<String, WidgetBuilder> _routeBuilders(BuildContext context) {
     return {
       // When called will output a route according to the index button pressed
-      "0": (context) => const HomePage(
+      "0": (context) => HomePage(
             title: 'Home', // Assign Homepage to index 0 and so on
           ),
       "1": (context) => const Testingt(),

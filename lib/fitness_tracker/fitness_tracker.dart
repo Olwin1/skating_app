b502dc73634e0d1d7ae41c84c09d52012fe3f376
Oliver Munn <oliver_munn@icloud.com>
2023-01-19 12:48:57 +0000

    Created basic distance traveled

--- lib/fitness_tracker/fitness_tracker.dart
+++ lib/fitness_tracker/fitness_tracker.dart
@@ -10,20 +10,34 @@ class FitnessTracker extends StatelessWidget {
   Widget build(BuildContext context) {
     User user = User("1"); // Create user object with id of 1
     return Scaffold(
-        body: Column(
-      // Create basic vertical layout
-      children: [
-        Container(), // Distance traveled
-        Expanded(
-          child: GridView.count(
-            // Create Basic Gridview
-            crossAxisCount: 2, // Set width to 2 widgets
-            children: [Container(), Container(), Container(), Container()],
-          ),
-        ),
-        Container(), // Speedometer button
-        Container() // Start Button
-      ],
-    ));
+        body: Padding(
+            padding: const EdgeInsets.all(32),
+            child: Column(
+              // Create basic vertical layout
+              children: [
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(8), // Add padding
+                  child: Column(children: const [
+                    Text("Distance Traveled"), // Distance Traveled Header
+                    Text("0.0 Km") // Distance Traveled Data
+                  ]),
+                ), // Distance traveled
+                Expanded(
+                  child: GridView.count(
+                    // Create Basic Gridview
+                    crossAxisCount: 2, // Set width to 2 widgets
+                    children: [
+                      Container(),
+                      Container(),
+                      Container(),
+                      Container()
+                    ],
+                  ),
+                ),
+                Container(), // Speedometer button
+                Container() // Start Button
+              ],
+            )));
   }
 }

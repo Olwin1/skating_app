debed4965c0c3341336fe9f341ed3087f1e9d867
Oliver Munn <oliver_munn@icloud.com>
2023-01-19 14:06:41 +0000

    Added comments

--- lib/fitness_tracker/fitness_tracker.dart
+++ lib/fitness_tracker/fitness_tracker.dart
@@ -2,68 +2,72 @@ import 'package:flutter/material.dart';
 import 'package:skating_app/objects/user.dart';
 
 class FitnessTracker extends StatelessWidget {
-  // Create FitnessTracker Class
+  // Constructor that takes a key and a title as required arguments
   const FitnessTracker({Key? key, required this.title}) : super(key: key);
-  final String title; // Define title argument
+  final String title;
 
-  @override // Override the existing widget build method
+  @override
   Widget build(BuildContext context) {
-    User user = User("1"); // Create user object with id of 1
+    // create an instance of the User class and passing it an id of '1'
+    User user = User("1");
     return Scaffold(
+        // Scaffold widget, which is the basic layout element in Flutter
         body: Padding(
+            // Padding widget to add padding around the edges of the screen
             padding: const EdgeInsets.all(32),
             child: Column(
-              // Create basic vertical layout
               children: [
+                // Spacer widget with a flex of 1, which is used to take up any remaining space in the Column.
                 const Spacer(
                   flex: 1,
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(32), // Add padding
+                  // Padding widget with smaller padding value, used to add padding around a nested Column widget
+                  padding: const EdgeInsets.all(32),
                   child: Column(children: const [
-                    Text("Distance Traveled"), // Distance Traveled Header
-                    Text("0.0 Km") // Distance Traveled Data
+                    Text("Distance Traveled"),
+                    Text("0.0 Km")
                   ]),
-                ), // Distance traveled
+                ),
                 Expanded(
+                  // used to take up any remaining space in the Column
                   child: GridView.count(
-                    // Create Basic Gridview
+                    // padding around the grid
                     padding: const EdgeInsets.symmetric(vertical: 16),
-                    crossAxisCount: 2, // Set width to 2 widgets
+                    // number of columns in the grid
+                    crossAxisCount: 2,
                     children: [
                       Column(children: const [
-                        Text("Session Duration"), // Session Duration Header
-                        Text("47s") // Session duration Data
+                        Text("Session Duration"),
+                        Text("47s")
                       ]),
                       Column(children: const [
-                        Text(
-                            "Average Session Duration"), // Average Session Duration Header
-                        Text("5:00") // Average Session duration Data
+                        Text("Average Session Duration"),
+                        Text("5:00")
                       ]),
+                      Column(
+                          children: const [Text("Sunset Time"), Text("19:45")]),
                       Column(children: const [
-                        Text("Sunset Time"), // Sunset Time Header
-                        Text("19:45") // Sunset Time Data
-                      ]),
-                      Column(children: const [
-                        Text("Average Speed"), // Average Speed Header
-                        Text("2kph") // Session duration Data
+                        Text("Average Speed"),
+                        Text("2kph")
                       ]),
                     ],
                   ),
                 ),
+                // TextButton widget with a callback function to print "pressed" when clicked
                 TextButton(
-                  // Create speedometer button
-                  onPressed: () => print("pressed"), // When pressed
-                  child: const Text("Speedometer"), // Speedometer filler text
-                ), // Speedometer button
+                  onPressed: () => print("pressed"),
+                  child: const Text("Speedometer"),
+                ),
                 Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 32), // Add padding to space away from rest
+                  // Padding widget to add padding around the button
+                  padding: const EdgeInsets.symmetric(vertical: 32),
                   child: TextButton(
+                    // callback function to print "pressed" when clicked
                     onPressed: () => print("pressed"),
-                    child: const Text("Start"), // Filler text
+                    child: const Text("Start"),
                   ),
-                ) // Start Button
+                )
               ],
             )));
   }

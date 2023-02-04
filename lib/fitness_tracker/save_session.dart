f0d2e491cb504d68806300df5db7b16f9abb89a0
Oliver Munn <oliver_munn@icloud.com>
2023-02-04 15:13:54 +0000

    Added basic layout of top 2

--- lib/fitness_tracker/save_session.dart
+++ lib/fitness_tracker/save_session.dart
@@ -21,6 +21,47 @@ class _SaveSession extends State<SaveSession> {
             child: const Text("Save Session"),
           ),
         ),
-        body: Container());
+        body: Padding(
+            padding: const EdgeInsets.all(48),
+            child: Column(
+              // Split layout into individual rows
+              children: [
+                Row(
+                  mainAxisAlignment:
+                      MainAxisAlignment.center, // Center Children
+                  // Top 2 elements
+                  children: [
+                    Container(
+                      // Distance Traveled Box
+                      margin: const EdgeInsets.all(8),
+                      child: Column(
+                        children: const [
+                          Text("Distance Traveled"), // Title
+                          Text("13km") // Value
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    Container(
+                      // Session Duration Box
+                      margin: const EdgeInsets.all(8),
+                      child: Column(
+                        children: const [
+                          Text("Session Duration"), // Title
+                          Text("1:03") // Value
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const Text("Session Name"), // Session Name Infobox
+                const Text(
+                    "Session Description"), // Session Description Infobox
+                const Text("Add Photos"), // Add Photos Infobox
+                const Text("Type"), // Session Type Infobox
+                const Text("Share To"), // Share to Infobox
+                const Text("Save Session"), // Save Session Infobox
+              ],
+            )));
   }
 }

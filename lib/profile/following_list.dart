fa44c3ab5addfab5b9f8b0d1dba43f1e5cec35b3
Oliver Munn <oliver_munn@icloud.com>
2023-02-16 18:26:14 +0000

    Added transition

---
+++ lib/profile/following_list.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:skating_app/objects/user.dart';
+import 'package:skating_app/profile/user_list_widget.dart';
+
+// FollowingList widget
+class FollowingList extends StatefulWidget {
+  const FollowingList({Key? key}) : super(key: key);
+
+  @override
+  State<FollowingList> createState() => _FollowingList();
+}
+
+class _FollowingList extends State<FollowingList> {
+  @override
+  Widget build(BuildContext context) {
+    // Creating a user object
+    User user = User("1");
+
+    return ListView(
+      // Returns a ListView widget
+      padding: const EdgeInsets.all(8),
+      children: const [
+        // A list of UserListWidget widgets
+        UserListWidget(title: "title"),
+        UserListWidget(title: "title"),
+        UserListWidget(title: "title"),
+        UserListWidget(title: "title"),
+        UserListWidget(title: "title"),
+        UserListWidget(title: "title"),
+        UserListWidget(title: "title"),
+        UserListWidget(title: "title"),
+        UserListWidget(title: "title"),
+      ],
+    );
+  }
+}
--- lib/profile/lists.dart
+++ lib/profile/lists.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:skating_app/objects/user.dart';
+import 'package:skating_app/profile/followers_list.dart';
+import 'package:skating_app/profile/following_list.dart';
 import 'package:skating_app/profile/user_list_widget.dart';
 import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 
 // Defining some constants for the SnakeNavigationBar
 const SnakeShape snakeShape = SnakeShape.circle;
 const Color selectedColor = Color(0xff4343cd);
-int _selectedItemPosition = 2;
 
 // Lists widget
 class Lists extends StatefulWidget {
@@ -19,64 +20,77 @@ class Lists extends StatefulWidget {
 }
 
 class _Lists extends State<Lists> {
+  int _selectedItemPosition = 2;
+
   @override
   Widget build(BuildContext context) {
     // Creating a user object
     User user = User("1");
 
     return Scaffold(
-        appBar: AppBar(
-            // Setting the title of the AppBar
-            title: const Text("username"),
-            // Setting the bottom of the AppBar
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(48.0),
-              child: SnakeNavigationBar.color(
-                // Setting the color of the SnakeView
-                snakeViewColor: selectedColor,
-                // Setting the color of the selected item
-                selectedItemColor: const Color(0xff4d3d2d),
-                // Setting the color of the unselected item
-                unselectedItemColor: Colors.blueGrey,
-                // Setting the behaviour of the SnakeBar
-                behaviour: SnakeBarBehaviour.pinned,
-                // Setting the shape of the Snake
-                snakeShape: const SnakeShape(
-                  shape: RoundedRectangleBorder(),
-                  padding: EdgeInsets.only(right: 10),
-                  height: 3,
-                ),
-                // Setting the padding of the SnakeBar
-                padding: EdgeInsets.zero,
-                // Setting whether to show unselected labels
-                showUnselectedLabels: true,
-                // Setting whether to show selected labels
-                showSelectedLabels: true,
-                // Setting the current index of the SnakeBar
-                currentIndex: _selectedItemPosition,
-                // Setting the onTap function of the SnakeBar
-                onTap: (index) => setState(() => _selectedItemPosition = index),
-                // Setting the items of the SnakeBar
-                items: const [
-                  BottomNavigationBarItem(icon: Text("Followers")),
-                  BottomNavigationBarItem(icon: Text("Following")),
-                ],
+      appBar: AppBar(
+          // Setting the title of the AppBar
+          title: const Text("username"),
+          // Setting the bottom of the AppBar
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(48.0),
+            child: SnakeNavigationBar.color(
+              // Setting the color of the SnakeView
+              snakeViewColor: selectedColor,
+              // Setting the color of the selected item
+              selectedItemColor: const Color(0xff4d3d2d),
+              // Setting the color of the unselected item
+              unselectedItemColor: Colors.blueGrey,
+              // Setting the behaviour of the SnakeBar
+              behaviour: SnakeBarBehaviour.pinned,
+              // Setting the shape of the Snake
+              snakeShape: const SnakeShape(
+                shape: RoundedRectangleBorder(),
+                padding: EdgeInsets.only(right: 10),
+                height: 3,
               ),
-            )),
-        // Setting the body of the scaffold to a ListView with some UserListWidgets
-        body: ListView(
-          padding: const EdgeInsets.all(8),
-          children: const [
-            UserListWidget(title: "title"),
-            UserListWidget(title: "title"),
-            UserListWidget(title: "title"),
-            UserListWidget(title: "title"),
-            UserListWidget(title: "title"),
-            UserListWidget(title: "title"),
-            UserListWidget(title: "title"),
-            UserListWidget(title: "title"),
-            UserListWidget(title: "title"),
-          ],
-        ));
+              // Setting the padding of the SnakeBar
+              padding: EdgeInsets.zero,
+              // Setting whether to show unselected labels
+              showUnselectedLabels: true,
+              // Setting whether to show selected labels
+              showSelectedLabels: true,
+              // Setting the current index of the SnakeBar
+              currentIndex: _selectedItemPosition,
+              // Setting the onTap function of the SnakeBar
+              onTap: (index) => {
+                setState(() => {_selectedItemPosition = index})
+              },
+              // Setting the items of the SnakeBar
+              items: const [
+                BottomNavigationBarItem(icon: Text("Followers")),
+                BottomNavigationBarItem(icon: Text("Following")),
+              ],
+            ),
+          )),
+      // Setting the body of the scaffold to a ListView with some UserListWidgets
+      body: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 800), // Duration for animation
+          transitionBuilder: (child, animation) {
+            // Function to build the transition
+            // Define the offset animation using a Tween and the provided animation
+            final offsetAnimation = Tween(
+              begin: const Offset(1.5, 0.0), // Starting position for the child
+              end: const Offset(0.0, 0.0), // Ending position for the child
+            ).animate(animation);
+            // Return the child wrapped in a ClipRect and SlideTransition
+            return ClipRect(
+              // Use the offset animation to position the child
+              child: SlideTransition(
+                position: offsetAnimation,
+                child: child,
+              ),
+            );
+          },
+          child: _selectedItemPosition == 1 // The child widget to animate
+              ? const FollowersList() // If _selectedItemPosition is 1, show the FollowersList
+              : const FollowingList() // Otherwise, show the FollowingList
+          ),
+    );
   }
 }

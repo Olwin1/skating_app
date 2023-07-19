2cb2d9e716c3d34f29efd47439c80f21201b84d4
Oliver Munn <oliver_munn@icloud.com>
2023-07-19 11:58:36 +0100

    Made user connections clickable

--- lib/profile/user_list_widget.dart
+++ lib/profile/user_list_widget.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:patinka/profile/profile_page.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:patinka/common_logger.dart';
 import 'package:patinka/objects/user.dart';
@@ -24,6 +25,14 @@ class UserListWidget extends StatefulWidget {
 // _UserListWidget class is the state of the UserListWidget
 class _UserListWidget extends State<UserListWidget> {
   Map<String, dynamic>? user;
+  handlePress() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => Profile(
+                  userId: user?["_id"],
+                )));
+  }
 
   // Builds the widget
   @override
@@ -36,56 +45,59 @@ class _UserListWidget extends State<UserListWidget> {
           : null);
     }
     // Returns a row with a CircleAvatar, a text widget, and a TextButton
-    return Container(
-        decoration: BoxDecoration(
-          color: const Color(0xbb000000),
-          borderRadius: BorderRadius.circular(8),
-        ),
-        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-        padding: const EdgeInsets.symmetric(vertical: 4),
-        child: Row(
-          children: [
-            // CircleAvatar with a radius of 26 and a background image from the assets folder
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-              child: user?["avatar"] == null
-                  ? Shimmer.fromColors(
-                      baseColor: const Color(0x66000000),
-                      highlightColor: const Color(0xff444444),
-                      child: CircleAvatar(
-                        // Create a circular avatar icon
-                        radius: 26, // Set radius to 36
-                        backgroundColor: swatch[900]!,
-                      ))
-                  : CachedNetworkImage(
-                      imageUrl:
-                          '${Config.uri}/image/thumbnail/${user!["avatar"]}',
-                      imageBuilder: (context, imageProvider) => Container(
-                        height: 52,
-                        width: 52,
-                        decoration: BoxDecoration(
-                          shape: BoxShape
-                              .circle, // Set the shape of the container to a circle
-                          image: DecorationImage(
-                              image: imageProvider, fit: BoxFit.cover),
+    return GestureDetector(
+        onTap: () => handlePress(),
+        child: Container(
+            decoration: BoxDecoration(
+              color: const Color(0xbb000000),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            padding: const EdgeInsets.symmetric(vertical: 4),
+            child: Row(
+              children: [
+                // CircleAvatar with a radius of 26 and a background image from the assets folder
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                  child: user?["avatar"] == null
+                      ? Shimmer.fromColors(
+                          baseColor: const Color(0x66000000),
+                          highlightColor: const Color(0xff444444),
+                          child: CircleAvatar(
+                            // Create a circular avatar icon
+                            radius: 26, // Set radius to 36
+                            backgroundColor: swatch[900]!,
+                          ))
+                      : CachedNetworkImage(
+                          imageUrl:
+                              '${Config.uri}/image/thumbnail/${user!["avatar"]}',
+                          imageBuilder: (context, imageProvider) => Container(
+                            height: 52,
+                            width: 52,
+                            decoration: BoxDecoration(
+                              shape: BoxShape
+                                  .circle, // Set the shape of the container to a circle
+                              image: DecorationImage(
+                                  image: imageProvider, fit: BoxFit.cover),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-            ),
-            // Text widget with the text "username"
-            Text(
-              user?["username"] ?? AppLocalizations.of(context)!.username,
-              style: TextStyle(color: swatch[701]),
-            ),
-            const Spacer(
-              flex: 10,
-            ),
-            // TextButton with an onPressed function that prints test value "ee" and a child text widget with the text "Follow"
-            TextButton(
-                onPressed: () => commonLogger.i("pressed"),
-                child: Text(AppLocalizations.of(context)!.follow)),
-            const Spacer()
-          ],
-        ));
+                ),
+                // Text widget with the text "username"
+                Text(
+                  user?["username"] ?? AppLocalizations.of(context)!.username,
+                  style: TextStyle(color: swatch[701]),
+                ),
+                const Spacer(
+                  flex: 10,
+                ),
+                // TextButton with an onPressed function that prints test value "ee" and a child text widget with the text "Follow"
+                TextButton(
+                    onPressed: () => commonLogger.i("pressed"),
+                    child: Text(AppLocalizations.of(context)!.follow)),
+                const Spacer()
+              ],
+            )));
   }
 }

54967e03b5ab5ea5e45ae75598d3c12c850cb9f5
Oliver Munn <oliver_munn@icloud.com>
2023-07-20 19:19:33 +0100

    Changed config to link to prod api if running prod

--- lib/api/config.dart
+++ lib/api/config.dart
@@ -1,6 +1,13 @@
 import 'dart:io' show Platform;
 
+import 'package:flutter/foundation.dart';
+
 class Config {
-  static String uri =
-      Platform.isAndroid ? 'http://10.0.2.2:4000' : 'http://localhost:4000';
+  // static String uri =
+  //     Platform.isAndroid ? 'http://10.0.2.2:4000' : 'http://localhost:4000';
+  static String uri = kDebugMode
+      ? Platform.isAndroid
+          ? 'http://10.0.2.2:4000'
+          : 'http://localhost:4000'
+      : "https://sabreguild.com:4000";
 }

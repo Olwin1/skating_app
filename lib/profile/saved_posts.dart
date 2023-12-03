df8aba5bce225a6ce6f8435cf36cf2ed7a9b1662
Oliver Munn <oliver_munn@icloud.com>
2023-12-03 19:02:08 +0000

    Improved commenting

--- lib/profile/saved_posts.dart
+++ lib/profile/saved_posts.dart
@@ -1,27 +1,28 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_overlay/flutter_overlay.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:patinka/api/social.dart';
 import 'package:patinka/common_logger.dart';
 import '../api/config.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../components/list_error.dart';
 import '../swatch.dart';
 
+// Variable to store the currently viewed image
 String? currentImage;
 
 // Define a new StatelessWidget called SavedPosts
 class SavedPosts extends StatelessWidget {
-  // Constructor for SavedPosts, which calls the constructor for its superclass (StatelessWidget)
+  // Constructor for SavedPosts
   const SavedPosts({super.key});
 
   // Override the build method of StatelessWidget to return a Consumer widget
   @override
   Widget build(BuildContext context) {
+    // Function to create a dialog widget for image viewing
     GestureDetector dialog() {
       return GestureDetector(
         onTap: () {
@@ -35,112 +36,129 @@ class SavedPosts extends StatelessWidget {
                   imageUrl: '${Config.uri}/image/$currentImage',
                   imageBuilder: (context, imageProvider) => Container(
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(
-                          8), // Set the shape of the container to a circle
+                      borderRadius: BorderRadius.circular(8),
                       image: DecorationImage(
-                          image: imageProvider, fit: BoxFit.contain),
+                        image: imageProvider,
+                        fit: BoxFit.contain,
+                      ),
                     ),
                   ),
-                ))
+                ),
+              )
             : const SizedBox.shrink(),
       );
     }
 
+    // Function to show the overlay with the image dialog
     void show() {
       HiOverlay.show(
         context,
         child: dialog(),
       ).then((value) {
-        commonLogger.t('Recieved value: $value');
+        commonLogger.t('Received value: $value');
       });
     }
 
     return Scaffold(
-        backgroundColor: const Color(0x77000000),
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          iconTheme: IconThemeData(color: swatch[701]),
-          elevation: 8,
-          shadowColor: Colors.green.shade900,
-          backgroundColor: Config.appbarColour,
-          foregroundColor: Colors.transparent,
-          systemOverlayStyle: const SystemUiOverlayStyle(
-            statusBarColor: Colors.transparent,
-            statusBarIconBrightness: Brightness.light,
-          ),
-          // Create appBar widget
-          title: Text("Saved Posts",
-              style: TextStyle(color: swatch[701])), // Set title
+      backgroundColor: const Color(0x77000000),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        // App bar styling
+        iconTheme: IconThemeData(color: swatch[701]),
+        elevation: 8,
+        shadowColor: Colors.green.shade900,
+        backgroundColor: Config.appbarColour,
+        foregroundColor: Colors.transparent,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.light,
         ),
-        // Basic list layout element
-        body: SavedPostsList(imageViewerController: show));
+        title: Text("Saved Posts", style: TextStyle(color: swatch[701])),
+      ),
+      body: SavedPostsList(imageViewerController: show),
+    );
   }
 }
 
-// Creates a grid tile widget from an image URL
-Widget _createGridTileWidget(Map<String, dynamic> post,
-    dynamic imageViewerController, Function refreshPage) {
+// Function to create a grid tile widget from an image URL
+Widget _createGridTileWidget(
+  Map<String, dynamic> post,
+  dynamic imageViewerController,
+  Function refreshPage,
+) {
   return Builder(builder: (context) {
+    // Function to pop the navigator
     void popNavigator() {
       Navigator.of(context).pop();
     }
 
     return GestureDetector(
       onLongPress: () async {
+        // Show confirmation dialog on long press
         await showDialog(
           useRootNavigator: false,
           context: context,
           builder: (BuildContext context) {
             return AlertDialog(
-                backgroundColor: swatch[800],
-                title: Text(
-                  'Are you sure you want to unsave this post?',
-                  style: TextStyle(color: swatch[701]),
-                ),
-                content: SizedBox(
-                    height: 96,
-                    child: Column(children: [
-                      TextButton(
-                        onPressed: () async {
-                          await SocialAPI.unsavePost(post["post_id"]);
-                          refreshPage();
-                          popNavigator();
-                        },
-                        child: Text(
-                          'Unsave',
-                          style: TextStyle(color: swatch[901]),
-                        ),
+              backgroundColor: swatch[800],
+              title: Text(
+                'Are you sure you want to unsave this post?',
+                style: TextStyle(color: swatch[701]),
+              ),
+              content: SizedBox(
+                height: 96,
+                child: Column(
+                  children: [
+                    TextButton(
+                      onPressed: () async {
+                        // Call API to unsave the post
+                        await SocialAPI.unsavePost(post["post_id"]);
+                        refreshPage();
+                        popNavigator();
+                      },
+                      child: Text(
+                        'Unsave',
+                        style: TextStyle(color: swatch[901]),
                       ),
-                      TextButton(
-                        onPressed: () async {
-                          popNavigator();
-                        },
-                        child: Text(
-                          'Cancel',
-                          style: TextStyle(color: swatch[901]),
-                        ),
-                      )
-                    ])));
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        popNavigator();
+                      },
+                      child: Text(
+                        'Cancel',
+                        style: TextStyle(color: swatch[901]),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
           },
         );
       },
-      //onLongPressEnd: (details) => _popupDialog?.remove(),
-      onTap: () => {currentImage = post["image"], imageViewerController()},
+      onTap: () => {
+        currentImage = post["image"],
+        imageViewerController(),
+      },
       child: CachedNetworkImage(
         imageUrl: '${Config.uri}/image/thumbnail/${post["image"]}',
         fit: BoxFit.cover,
         imageBuilder: (context, imageProvider) => Container(
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(
-                8), // Set the shape of the container to a circle
-            image: DecorationImage(image: imageProvider, fit: BoxFit.contain),
+            borderRadius: BorderRadius.circular(8),
+            image: DecorationImage(
+              image: imageProvider,
+              fit: BoxFit.contain,
+            ),
           ),
         ),
-      ), // Display the image from the URL
+      ),
     );
   });
 }
 
+// Class representing the SavedPostsList widget
 class SavedPostsList extends StatefulWidget {
   final dynamic imageViewerController;
   const SavedPostsList({super.key, required this.imageViewerController});
@@ -150,45 +168,33 @@ class SavedPostsList extends StatefulWidget {
 }
 
 class _SavedPostsListState extends State<SavedPostsList> {
-  // Define the page size used for pagination
+  // Page size used for pagination
   static const _pageSize = 20;
 
-  // Create a PagingController to manage pagination
+  // PagingController to manage pagination
   final PagingController<int, Map<String, dynamic>> _pagingController =
       PagingController(firstPageKey: 0);
 
+  // Function to create loading widgets for the grid
   Widget _createGridLoadingWidgets() {
     Widget child = Shimmer.fromColors(
-        baseColor: shimmer["base"]!,
-        highlightColor: shimmer["highlight"]!,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(8.0),
-          child: Image.asset("assets/placeholders/150.png"),
-        ));
+      baseColor: shimmer["base"]!,
+      highlightColor: shimmer["highlight"]!,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: Image.asset("assets/placeholders/150.png"),
+      ),
+    );
     return GridView(
       shrinkWrap: true,
       physics: const ScrollPhysics(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        // Specify the properties for the grid tiles
         childAspectRatio: 1,
         mainAxisSpacing: 2,
         crossAxisSpacing: 2,
         maxCrossAxisExtent: MediaQuery.of(context).size.width / 3,
       ),
-      children: [
-        child,
-        child,
-        child,
-        child,
-        child,
-        child,
-        child,
-        child,
-        child,
-        child,
-        child,
-        child
-      ],
+      children: List.generate(12, (index) => child),
     );
   }
 
@@ -201,10 +207,12 @@ class _SavedPostsListState extends State<SavedPostsList> {
     super.initState();
   }
 
+  // Function to refresh the page
   void refreshPage() {
     _pagingController.refresh();
   }
 
+  // Function to fetch a page of posts
   Future<void> _fetchPage(int pageKey) async {
     try {
       // Fetch the next page of posts from the user's account
@@ -222,16 +230,14 @@ class _SavedPostsListState extends State<SavedPostsList> {
         } else {
           int rem =
               4 - ((_pagingController.itemList?.length ?? 0) + page.length) % 3;
-          List<Map<String, dynamic>> spacers = [];
-          for (int i = 0; i < rem; i++) {
-            spacers.add({"last": true});
-          }
+          List<Map<String, dynamic>> spacers =
+              List.generate(rem, (index) => {"last": true});
           _pagingController.appendLastPage([...page, ...spacers]);
         }
       } else if (mounted) {
         // If there are more pages of posts, append the current page to the PagingController
         // and specify the key for the next page
-        final nextPageKey = pageKey += 1;
+        final nextPageKey = pageKey + 1;
         _pagingController.appendPage(page, nextPageKey);
       }
     } catch (error) {
@@ -247,7 +253,6 @@ class _SavedPostsListState extends State<SavedPostsList> {
       physics: const ScrollPhysics(),
       pagingController: _pagingController,
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        // Specify the properties for the grid tiles
         childAspectRatio: 1,
         mainAxisSpacing: 2,
         crossAxisSpacing: 2,
@@ -258,7 +263,7 @@ class _SavedPostsListState extends State<SavedPostsList> {
             _createGridLoadingWidgets(),
         noItemsFoundIndicatorBuilder: (context) =>
             const ListError(title: "No Posts Saved", body: ""),
-        // Specify how to build each grid tile
+        // Build each grid tile
         itemBuilder: (context, item, index) => item["last"] == true
             ? const SizedBox(
                 height: 72,
@@ -275,7 +280,7 @@ class _SavedPostsListState extends State<SavedPostsList> {
       // Dispose of the PagingController when the state object is disposed
       _pagingController.dispose();
     } catch (e) {
-      commonLogger.e("An error has occured: $e");
+      commonLogger.e("An error has occurred: $e");
     }
     super.dispose();
   }

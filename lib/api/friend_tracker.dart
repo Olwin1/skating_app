a39f9a17fc1d1eafe4946e57da7d820305b6e80d
Olwin1 <oliver_munn@icloud.com>
2024-07-19 06:11:24 +0100

    Fixed comments

--- lib/api/friend_tracker.dart
+++ lib/api/friend_tracker.dart
@@ -1,44 +1,53 @@
-// Import necessary libraries and files
-import 'package:patinka/api/response_handler.dart';
-import 'package:patinka/common_logger.dart';
-import 'package:http/http.dart' as http;
-import 'config.dart';
-import 'dart:convert';
+// Import necessary packages
+import 'package:patinka/api/response_handler.dart'; // Custom response handler
+import 'package:patinka/common_logger.dart'; // Custom logger for logging
+import 'package:http/http.dart' as http; // HTTP client for making requests
+import 'config.dart'; // Configuration file for the application
+import 'dart:convert'; // For JSON encoding and decoding
 
-// Define a class for handling API requests related to messages
+// Class for handling API requests related to messages
 class MessagesAPI {
-  // Define URIs for different API endpoints
+  // Define the base URL for the search API
   static final Uri _searchUrl = Uri.parse('${Config.uri}/location/search');
 
-  // Define a static method to check if the user is friends with another user
+  // Function to perform a search with given terms
   static Future<Map<String, dynamic>> search(Map<String, String> terms) async {
     try {
+      // Initialize an empty map to hold the processed search terms
       Map<String, String> searchTerms = {};
+
+      // Process each term in the input map
       for (String term in terms.keys) {
         if (terms[term] != null) {
+          // Remove colons and quotes from the search term and its value
           searchTerms[term.replaceFirst(":", "")] = terms[term]!
               .replaceFirst(term, "")
               .replaceFirst('"', '')
               .replaceFirst('"', '');
         }
       }
-      // Send a GET request to the friend check URL with user information
+
+      // Make a GET request to the search API
       var response = await http.get(_searchUrl, headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Bearer ${await storage.getToken()}',
-        'terms': jsonEncode(searchTerms)
+        'Content-Type': 'application/x-www-form-urlencoded', // Set content type
+        'Authorization':
+            'Bearer ${await storage.getToken()}', // Add authorization token
+        'terms': jsonEncode(
+            searchTerms) // Add the search terms as a JSON-encoded string
       });
 
-      // Check the response status code
+      // Check if the response status is OK (200)
       if (response.statusCode == 200) {
-        commonLogger.t("Response: 200 Search Successful");
-
-        // Parse the response body and return the result
-        return handleResponse(response, Resp.stringResponse);
+        commonLogger
+            .t("Response: 200 Search Successful"); // Log success message
+        return handleResponse(
+            response, Resp.stringResponse); // Handle and return the response
       }
+
+      // Throw an exception if the search was unsuccessful
       throw Exception("Search Unsuccessful: ${response.reasonPhrase}");
     } catch (e) {
-      // Throw an exception if there's an error during the friend check process
+      // Catch and throw any errors that occur during the search process
       throw Exception("Error during search: $e");
     }
   }

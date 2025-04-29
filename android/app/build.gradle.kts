1c3693421a599173128303bb461498bea3dbfecb
Olwin1 <oliver_munn@icloud.com>
2025-04-29 23:22:53 +0100

    Updated build.gradle

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("com.android.application")
+// START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+// END: FlutterFire Configuration
+
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.olwin.patinka"
+    compileSdk = flutter.compileSdkVersion // 33
+    ndkVersion = flutter.ndkVersion // 23.1.7779620
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.olwin.patinka"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://docs.flutter.dev/deployment/android#reviewing-the-build-configuration.
+        minSdk = 27 // 16
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+        multiDexEnabled = true
+    }
+    
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+// // Not sure about here?
+// dependencies {
+//     coreLibraryDesugaring = 'com.android.tools:desugar_jdk_libs:1.1.5'
+//     implementation = 'androidx.window:window:1.0.0'
+//     implementation 'androidx.window:window-java:1.0.0'
+//     implementation 'com.google.firebase:firebase-messaging:18.0.0' 
+// }

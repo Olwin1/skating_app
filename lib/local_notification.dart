0670cf215414c222bcc58d7974f70388aee7c70e
Olwin1 <oliver_munn@icloud.com>
2025-06-01 22:43:11 +0100

    Changed import

--- lib/local_notification.dart
+++ lib/local_notification.dart
@@ -6,9 +6,9 @@ import "package:patinka/api/fcm_token.dart";
 import "package:patinka/api/messages.dart";
 import "package:patinka/api/social.dart";
 import "package:patinka/common_logger.dart";
+import "package:patinka/misc/notifications/session_notification.dart";
 import "package:patinka/services/navigation_service.dart";
 import "package:patinka/social_media/private_messages/private_message.dart";
-import "package:patinka/misc/notifications/session_notification.dart";
 
 // Entry point for handling Firebase background messages
 @pragma("vm:entry-point")
